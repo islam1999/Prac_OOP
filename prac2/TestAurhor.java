45f47d3f4dfcdd06b23481a487f06fd65b60d89b
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-12-10 21:32:01 +0300

    Create TestAurhor.java

---
+++ prac2/TestAurhor.java
@@ -0,0 +1,13 @@
+package ru.mirea.prac2
+
+public class TestAuthor {
+
+    public static void main(String[] args) {
+
+        Author a = new Author("Uncle Bob", "unclebob@gmail.com", 'm');
+
+        System.out.println(a);
+
+    }
+
+}
