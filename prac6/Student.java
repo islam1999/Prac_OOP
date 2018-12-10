9fc203794a68c354503deff1f18f9265cccbc071
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-12-10 21:37:55 +0300

    Create Student.java

---
+++ prac6/Student.java
@@ -0,0 +1,37 @@
+package ru.mirea.prac6
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
+public class Student {
+    private int score;
+
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        System.out.println("Generated:");
+        for (int i = 0; i < 50; i++) {
+            students.add(new Student(new Random().nextInt(100)));
+            System.out.println(students.get(i));
+        }
+
+        System.out.println("===============================\nSorted:");
+        students.sort(Comparator.comparingInt(s -> s.score));
+        for (Student s : students) {
+            System.out.println(s);
+        }
+    }
+
+    public Student(int score) {
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student: #" + hashCode() + ", score: " + score;
+    }
+}
