f885afc1d6689d9bd504cd3625511ce847732472
islam1999 <31537821+islam1999@users.noreply.github.com>
2018-12-10 21:28:18 +0300

    Create Main.java

---
+++ prac1/Main.java
@@ -0,0 +1,13 @@
+package ru.mirea.prac1
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book b = new Book("Java Cookbook", "Ian F. Darwin", 864);
+
+        System.out.println(b);
+
+    }
+
+}
